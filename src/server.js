6ee63ab003cb3d323f74789239aa2c1e493e30a2
iwashi623 <wonderland.7878@gmail.com>
2023-08-07 13:06:45 +0900

    refactor

--- src/server.js
+++ src/server.js
@@ -1,63 +1,45 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer, gql } = require("apollo-server");
+const fs = require("fs");
+const path = require("path");
 
 // リンクオブジェクトの配列
 const links = [
-    {
-        id: 'link-0',
-        url: 'www.howtographql.com',
-        description: 'Fullstack tutorial for GraphQL',
-    },
+  {
+    id: "link-0",
+    url: "www.howtographql.com",
+    description: "Fullstack tutorial for GraphQL",
+  },
 ];
-
-// GraphQL schema定義
-const typeDefs = gql`
-    type Query {
-        info: String!
-        feed: [Link!]!
-    }
-
-    type Mutation {
-        post(url: String!, description: String!): Link!
-    }
-
-    type Link {
-        id: ID!
-        description: String!
-        url: String!
-    }
-`;
 
 // リゾルバー関数定義
 const resolvers = {
-    // infoフィールドのリゾルバー関数
-    Query: {
-        info: () => `This is the API of a Hackernews Clone`,
-        feed: () => links,
+  // infoフィールドのリゾルバー関数
+  Query: {
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+  },
+
+  Mutation: {
+    post: (parent, args) => {
+      let idCount = links.length;
+
+      const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      };
+
+      links.push(link);
+      return link;
     },
-
-    Mutation: {
-        post: (parent, args) => {
-            let idCount = links.length;
-
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
-                url: args.url,
-            };
-
-            links.push(link);
-            return link;
-        }
-    }
+  },
 };
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+  typeDefs: fs.readFileSync(path.join(__dirname, "schema.graphql"), "utf8"),
+  resolvers,
 });
 
-server
-    .listen()
-    .then(({ url }) => {
-        console.log(`Server is running on ${url}`);
-    });
+server.listen().then(({ url }) => {
+  console.log(`Server is running on ${url}`);
+});
